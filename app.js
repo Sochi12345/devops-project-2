0aefa133296d292adbf8a3e2389367e1ae7a4550
Sochi Alomefuna <blvn@Sochis-MacBook-Pro.local>
2026-03-27 16:20:17 +0100

    Initial commit - Node API app

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.json({ message: "DevOps Project 2 Running 🚀" });
+});
+
+app.get('/health', (req, res) => {
+  res.json({ status: "OK" });
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
